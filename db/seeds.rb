76346e96ed48345adbd66aa44b26cb70c35df96d
igor <zawgorodskiy.2012@gmail.com>
2017-12-26 16:14:49 +0200

    some fix and added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,3 +40,5 @@ sam_workshop_sec = second_sam_factory.workshops.create(name: 'Secondary workshop
 
 apple_workshop = first_apple_factory.workshops.create(name: 'Main workshop')
 apple_workshop_sec = second_apple_factory.workshops.create(name: 'Secondary workshop')
+
+first_lg_factory.create_principal(email: 'principal@1.com', password: 123123, name: John)
